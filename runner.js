4f50f46fc7665752ad48b8d4b0b4aff3416d1643
karl.hudgell <karl.hudgell@bjss.com>
2022-06-09 19:08:32 +0100

    updated runner code and logging

--- runner.js
+++ runner.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
 const { feedUpdater } = require('./FeedUpdater')
 const { filterFeed } = require('./FeedFilter')
+const log = require('simple-node-logger').createSimpleLogger('jdrssdownloader.log');
 
-setInterval(feedUpdater, 600000);
-setInterval(filterFeed, 18000000);
+let RSSFeedRefreshMins = JSON.parse(fs.readFileSync('config.json')).RSSFeedRefreshMins
+let JDPostLinksMins = JSON.parse(fs.readFileSync('config.json')).JDPostLinksMins
+
+log.info('Refreshing RSS Items every ' + RSSFeedRefreshMins + ' Minutes')
+log.info('Checking for links and sending to JDdownloader every ' + JDPostLinksMins + ' Minutes')
+
+setInterval(feedUpdater, RSSFeedRefreshMins * 60000);
+setInterval(filterFeed, JDPostLinksMins * 60000);
