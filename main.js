419031d65da0da8028905b72063ed739875ea0f6
karl.hudgell <karl.hudgell@bjss.com>
2022-06-09 10:06:22 +0100

    get links from source page

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 // Import dependencies
 const fs = require("fs");
 const Parser = require("rss-parser");
-const { filterFeed } = require("./feed");
+const { filterFeed } = require("./FeedFilter");
 const lodash = require('lodash');
 const log = require('simple-node-logger').createSimpleLogger('jdrssdownloader.log');
 
-    (async function main() {
+(async function main() {
 
 
-        // Make a new RSS Parser
-        const parser = new Parser();
+    // Make a new RSS Parser
+    const parser = new Parser();
 
-        // Get all the items in the RSS feed
-        const feed = await parser.parseURL(JSON.parse(fs.readFileSync('config.json')).RSSFeed);
+    // Get all the items in the RSS feed
+    const feed = await parser.parseURL(JSON.parse(fs.readFileSync('config.json')).RSSFeed);
 
-        let items = [];
+    let items = [];
 
-        // Clean up the string and replace reserved characters
-        const fileName = `${feed.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
+    // Clean up the string and replace reserved characters
+    const fileName = `${feed.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
 
-        if (fs.existsSync(fileName)) {
-            items = require(`./${fileName}`);
-        }
-        let updatedArray = lodash.unionBy(feed.items, items, 'title');
-        
-        // Save the file
-        log.info(updatedArray.length + ' items in file cache')
-        fs.writeFileSync(fileName, JSON.stringify(updatedArray));
+    if (fs.existsSync(fileName)) {
+        items = require(`./${fileName}`);
+    }
+    // Compare existing cache and new items and merge differences
+    let updatedArray = lodash.unionBy(feed.items, items, 'title');
 
-        // run next part
-        filterFeed(fileName)
-    })();
+    // Save the file
+    log.info(updatedArray.length + ' items in file cache')
+    fs.writeFileSync(fileName, JSON.stringify(updatedArray));
+
+    // run next part
+    filterFeed(fileName)
+})();
