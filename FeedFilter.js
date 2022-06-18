8b0040d8ddabf2ac98921db2c0c3d248b0f209e2
karl.hudgell <karl.hudgell@bjss.com>
2022-06-18 10:08:05 +0100

    download all if more than one post found

--- FeedFilter.js
+++ FeedFilter.js
@@ -17,41 +17,44 @@ async function filterFeed() {
             // Find show on feed
             let list_filtered_for_show = feed.filter(item => item.title.includes(show.Name))
             if (list_filtered_for_show.length > 0) {
-                // If show is found get url then return all links on that page
-                let full_link_list_from_page = await getLinksFromURL(list_filtered_for_show[0].link)
-                if (hevcSwitch) {
-                    // Only get urls with HEVC in name
-                    urls_to_check = full_link_list_from_page.filter(item => item.includes('HEVC'))
-                    if (urls_to_check.length == 0) {
-                        // If no urls with HEVC check for H265
-                        urls_to_check = full_link_list_from_page.filter(item => item.includes('H265'))
+                for (let match of list_filtered_for_show) {
+                    // If show is found get url then return all links on that page
+                    let full_link_list_from_page = await getLinksFromURL(match.link)
+                    if (hevcSwitch) {
+                        // Only get urls with HEVC in name
+                        urls_to_check = full_link_list_from_page.filter(item => item.includes('HEVC'))
+                        if (urls_to_check.length == 0) {
+                            // If no urls with HEVC check for H265
+                            urls_to_check = full_link_list_from_page.filter(item => item.includes('H265'))
+                        }
+                    } else {
+                        urls_to_check = full_link_list_from_page
                     }
-                } else {
-                    urls_to_check = full_link_list_from_page
-                }
-                // Only keep urls that match show quality
-                let urls_with_quality_in_url = urls_to_check.filter(item => item.includes(show.Quality))
-                // Remove any url trying to direct to a torrent site search
-                let urls_without_torrent_in_url = urls_with_quality_in_url.filter(item => !item.includes('torrent'))
-                // Remove any url that doesn't include MeGusta
-                if (hevcSwitch) {
-                    pre_nitroFlare = urls_without_torrent_in_url.filter(item => item.includes('MeGusta'))
-                } else {
-                    pre_nitroFlare = urls_without_torrent_in_url
-                }
-                // NitroFlare doesn't group with the rest of the links in JD, remove them.
-                let remove_nitroflare = pre_nitroFlare.filter(item => !item.includes('nitro'))
-                // Do some stuff
-                let download_list = checkFileName(remove_nitroflare)
-                // Send Links to JDdownloader
-                if (download_list.length !== 0) {
-                    log.info(download_list.length + ' links for ' + show.Name + ' have been sent to JDdownloader')
-                    linkAdder(download_list)
-                } else {
-                    // No HEVC links found
-                    log.info(download_list.length + ' links for ' + show.Name + ' have been found, will recheck next time.')
-                    for (let feed_item of list_filtered_for_show) {
-                        retry_show_cache.push(feed_item)
+                    // Only keep urls that match show quality
+                    let urls_with_quality_in_url = urls_to_check.filter(item => item.includes(show.Quality))
+                    // Remove any url trying to direct to a torrent site search
+                    let urls_without_torrent_in_url = urls_with_quality_in_url.filter(item => !item.includes('torrent'))
+                    // Remove any url that doesn't include MeGusta
+                    if (hevcSwitch) {
+                        pre_nitroFlare = urls_without_torrent_in_url.filter(item => item.includes('MeGusta'))
+                    } else {
+                        pre_nitroFlare = urls_without_torrent_in_url
+                    }
+                    // NitroFlare doesn't group with the rest of the links in JD, remove them.
+                    let remove_nitroflare = pre_nitroFlare.filter(item => !item.includes('nitro'))
+                    // Do some stuff
+                    urlObj = checkFileName(remove_nitroflare)
+                    let download_list = urlObj.urlList
+                    // Send Links to JDdownloader
+                    if (download_list.length !== 0) {
+                        log.info(download_list.length + ' links for ' + urlObj.fileName + ' have been sent to JDdownloader')
+                        linkAdder(download_list)
+                    } else {
+                        // No HEVC links found
+                        log.info(download_list.length + ' links for ' + show.Name + ' have been found, will recheck next time.')
+                        for (let feed_item of list_filtered_for_show) {
+                            retry_show_cache.push(feed_item)
+                        }
                     }
                 }
             } else {
