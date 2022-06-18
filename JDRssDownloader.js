fb0ad8af5e66f2cba9686ed2edb0e4bbe3e58af8
karl.hudgell <karl.hudgell@bjss.com>
2022-06-18 13:19:45 +0100

    print app version when starting

--- JDRssDownloader.js
+++ JDRssDownloader.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const { feedUpdater } = require('./FeedUpdater')
 const { filterFeed } = require('./FeedFilter')
+const version = require('./package.json').version;
+
 global.log = require('simple-node-logger').createSimpleLogger({
     logFilePath: 'jdrssdownloader.log',
     timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
@@ -10,6 +12,7 @@ async function main() {
     let RSSFeedRefreshMins = JSON.parse(fs.readFileSync('config.json')).RSSFeedRefreshMins
     let JDPostLinksMins = JSON.parse(fs.readFileSync('config.json')).JDPostLinksMins
 
+    log.info('Running JDRssDownloader version ' + version)
     log.info('Refreshing RSS Items every ' + RSSFeedRefreshMins + ' Minutes')
     log.info('Checking for links and sending to JDdownloader every ' + JDPostLinksMins + ' Minutes')
 
